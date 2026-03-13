6de035575b0a7314564f0ef8d4d59eab50c0b67d
Srimannarayana Sai Kadali <srimannarayana.kadali@techcarrot.ae>
2026-03-13 23:20:59 +0530

    monetary field for manual payroll

--- techcarrot_employee/models/tec_contract.py
+++ techcarrot_employee/models/tec_contract.py
@@ -39,13 +39,13 @@ class HrVersionInherit(models.Model):
 
     aat_allowance = fields.Monetary('MI Allowance', copy=False)
     sub_total = fields.Monetary('Sub Total', copy=False)
-    basic_salary_manual = fields.Float(string="Basic Salary Manual")
-    hra_manual = fields.Float(string="HRA Manual")
-    flexi_manual = fields.Float(string="Flexi Basket Manual")
-    statutory_manual = fields.Float(string="Statutory Bonus Manual")
-    gratuity_manual = fields.Float(string="Gratuity Manual")
-    pf_manual = fields.Float(string="PF Manual")
-    medical_manual=fields.Float(string="Medical Insurance Manual")
+    basic_salary_manual = fields.Monetary(string="Basic Salary Manual")
+    hra_manual = fields.Monetary(string="HRA Manual")
+    flexi_manual = fields.Monetary(string="Flexi Basket Manual")
+    statutory_manual = fields.Monetary(string="Statutory Bonus Manual")
+    gratuity_manual = fields.Monetary(string="Gratuity Manual")
+    pf_manual = fields.Monetary(string="PF Manual")
+    medical_manual=fields.Monetary(string="Medical Insurance Manual")
     # emp_code = fields.Char('Employee Code', copy=False)
     #
     #
